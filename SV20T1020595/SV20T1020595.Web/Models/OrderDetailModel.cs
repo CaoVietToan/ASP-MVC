1dbd840dced0a71aa4f7dbd1aa231737e7c0c08a
CaoVietToan <20t1020595@husc.edu.vn>
2024-06-12 05:02:47 +0700

    ASP.NET

---
+++ SV20T1020595/SV20T1020595.Web/Models/OrderDetailModel.cs
@@ -0,0 +1,10 @@
+﻿using SV20T1020595.DomainModels;
+
+namespace SV20T1020595.Web.Models
+{
+    public class OrderDetailModel
+    {
+        public Order Order { get; set; }
+        public List<OrderDetail> Details { get; set; }
+    }
+}
